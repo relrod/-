ca3c105723ecb2239d56d85eed2fc58d32db5a8f
Ricky Elrod <ricky@elrod.me>
2014-07-27 05:52:44 -0400

    add a small repl/file evaluator
    Signed-off-by: Ricky Elrod <ricky@elrod.me>

---
+++ src/Repl.hs
@@ -0,0 +1,38 @@
+module Main where
+
+import Control.Applicative
+import Control.Monad
+import Control.Monad.IO.Class
+import Data.Maybe (listToMaybe)
+import Data.Monoid
+import Language.Dash.Parser
+import Language.Dash.Evaluate
+import Text.Trifecta
+import System.Console.Haskeline
+import System.Environment (getArgs)
+import System.IO
+
+main :: IO ()
+main = do
+  filename <- listToMaybe <$> getArgs
+  case filename of
+    Just name -> readFile name >>= evalString
+    Nothing   -> repl
+
+repl :: IO ()
+repl = do
+  hSetBuffering stdout NoBuffering
+  putStrLn "λ Welcome to dash! λ"
+  runInputT defaultSettings loop
+  where
+    loop :: InputT IO ()
+    loop = forever $ do
+      minput <- getInputLine "dash> "
+      case minput of
+        Nothing     -> return ()
+        Just "quit" -> return ()
+        Just "exit" -> return ()
+        Just input  -> liftIO $ evalString input
+
+evalString :: String -> IO ()
+evalString s = print $ eval mempty <$> parseString expression mempty s

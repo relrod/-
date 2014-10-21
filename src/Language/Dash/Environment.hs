fd5b253b5144cc209e7c3f3686518e4014256e62
Ricky Elrod <ricky@elrod.me>
2014-10-21 02:24:26 -0400

    no
    Signed-off-by: Ricky Elrod <ricky@elrod.me>

--- src/Language/Dash/Environment.hs
+++ src/Language/Dash/Environment.hs
@@ -9,11 +9,14 @@ module Language.Dash.Environment (
   getEnv
   ) where
 
+--import Control.Applicative ((<$>))
+import Control.Monad
+--import Data.Bifunctor
 import Data.List.NonEmpty
 import Data.Monoid
 import Prelude
-  ((++), (+), (-), (>>=), Bool, Enum, Functor, Int, Maybe(..), Monad,
-   Show(show), String, error, fromEnum, toEnum, lookup, return)
+  ((.), (++), (+), (-), (>>=), ($), Bool, Enum, Functor, Int, Maybe(..), Monad,
+   Show(show), String, error, fmap, fromEnum, toEnum, lookup, return)
 
 data Environment = Environment [(String, Literal)] deriving (Show)
 
@@ -39,7 +42,7 @@ data Term a
   | Lambda a (Term a)
   | Literal Literal
   | If (Term a) (Term a) (Term a)
-  | LetRec (NonEmpty (a, (Term a))) (Term a)
+  | LetRec (NonEmpty (a, Term a)) (Term a)
   deriving (Functor, Show)
 
 instance Enum (Term String) where
@@ -49,6 +52,15 @@ instance Enum (Term String) where
     f (Variable _) i  = i
     f (Lambda _ t') i = f t' (i + 1)
     f _            _  = error "Not a church-encodable term"
+
+--instance Monad Term where
+--  return = Variable
+--  Variable a >>= f = f a
+--  Apply t1 t2 >>= f = Apply (t1 >>= f) (t2 >>= f)
+--  --Lambda a t >>= f = Lambda a (t >>= f)
+--  Literal x >>= f = Literal x
+--  If x y z >>= f = If (x >>= f) (y >>= f) (z >>= f)
+--  LetRec x y >>= f = LetRec (bindInner x f) (error "h")
 
 getEnv :: Environment -> String -> Maybe Literal
 getEnv (Environment e) s = lookup s e

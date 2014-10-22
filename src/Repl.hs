9ff6e5c9e75afbd91d7a27080ec67de461cd220d
Ricky Elrod <ricky@elrod.me>
2014-10-22 04:19:30 -0400

    Add persistent let bindings to the REPL
    Signed-off-by: Ricky Elrod <ricky@elrod.me>

--- src/Repl.hs
+++ src/Repl.hs
@@ -1,8 +1,13 @@
+{-# LANGUAGE ViewPatterns #-}
 module Main where
 
 import Control.Applicative
+import Control.Arrow hiding (loop)
 import Control.Monad
 import Control.Monad.IO.Class
+import Control.Monad.Trans
+import Control.Monad.Trans.State.Lazy
+import Data.List (stripPrefix)
 import Data.Maybe (listToMaybe)
 import Data.Monoid
 import Language.Dash.Parser
@@ -23,7 +28,7 @@ main :: IO ()
 main = do
   filename <- listToMaybe <$> getArgs
   case filename of
-    Just name -> readFile name >>= evalString
+    Just name -> readFile name >>= flip evalString' []
     Nothing   -> repl
 
 repl :: IO ()
@@ -34,21 +39,40 @@ repl = do
   homeDir <- getHomeDirectory
   runInputT defaultSettings {
     historyFile = Just (homeDir </> ".dashrepl_history")
-  } $ withInterrupt loop
+    } $ withInterrupt (evalStateT loop [])
   where
-    loop :: InputT IO ()
+    loop :: StateT [(String, Literal)] (InputT IO) ()
     loop = forever $ do
-      minput <- handleInterrupt (return Nothing) $ getInputLine "dash> "
+      minput <- lift $ handleInterrupt (return Nothing) $ getInputLine "dash> "
       case minput of
         Nothing     -> return ()
         Just "quit" -> liftIO exitSuccess
         Just "exit" -> liftIO exitSuccess
-        Just input  -> liftIO $ handleInterrupt (return ()) (evalString input)
+        Just input  -> evalString input --liftIO $ handleInterrupt (return ()) (evalString input)
 
-evalString :: String -> IO ()
-evalString s =
-  let evaled = runEval s
-  in putStrLn $ case evaled of
+evalString :: String -> StateT [(String, Literal)] (InputT IO) ()
+evalString ":let" = do
+  st <- get
+  liftIO . putStrLn $ show st
+evalString (stripPrefix ":let " -> Just newbinding) = do
+  let (name, binding) = second (dropWhile (==' ')) $ break (==' ') newbinding
+  st <- get
+  let evaled = runEval binding st
+  case evaled of
+    Success s' -> case s' of
+      Just y -> do
+        modify (\x -> (name, y) : x)
+        x <- get
+        liftIO . print $ x
+      Nothing -> return ()
+    Failure d -> liftIO . putStrLn $ show d
+evalString s = liftIO . evalString' s =<< get
+
+-- | Evaluate a String of dash code with some extra "stuff" in the environment.
+evalString' :: String -> [(String, Literal)] -> IO ()
+evalString' s st = do
+  let evaled = runEval s st
+  liftIO $ putStrLn $ case evaled of
     Success s' -> colorize (show s')
     Failure d -> show d
 
@@ -61,5 +85,5 @@ colorize =
     ""
     False
 
-runEval :: String -> Result (Maybe Literal)
-runEval s = eval mempty <$> parseString (runParser expression) mempty s
+runEval :: String -> [(String, Literal)] -> Result (Maybe Literal)
+runEval s env = eval (Environment env) <$> parseString (runParser expression) mempty s

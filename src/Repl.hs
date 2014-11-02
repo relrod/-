b6578cbcc75c5d7225433ebfd19de93fbefa0fc0
Ricky Elrod <ricky@elrod.me>
2014-11-02 00:54:54 -0400

    [dashrepl] Add a way to print out the parse tree
    Signed-off-by: Ricky Elrod <ricky@elrod.me>

--- src/Repl.hs
+++ src/Repl.hs
@@ -8,7 +8,6 @@ import Control.Monad.IO.Class
 import Control.Monad.Trans
 import Control.Monad.Trans.State.Lazy
 import Data.List (stripPrefix)
-import Data.Maybe (listToMaybe)
 import Data.Monoid
 import Language.Dash.Parser
 import Language.Dash.Environment
@@ -16,23 +15,47 @@ import Language.Dash.Evaluate
 import qualified Language.Haskell.HsColour as HsColour
 import qualified Language.Haskell.HsColour.Colourise as HsColour
 import qualified Language.Haskell.HsColour.Output as HsColour
-import Text.Trifecta
+import qualified Options.Applicative as OA
 import System.Console.Haskeline
 import System.Directory
-import System.Environment (getArgs)
 import System.Exit (exitSuccess)
 import System.FilePath ((</>))
 import System.IO
+import Text.Show.Pretty
+import Text.Trifecta
+
+data Arguments = Arguments
+  {
+    showParse :: Bool
+  , filename  :: Maybe FilePath
+  }
+
+parseArgs :: OA.Parser Arguments
+parseArgs = Arguments
+  <$> OA.switch
+      ( OA.long "show-parse"
+     <> OA.help "Show the parse tree of each input in addition to the result")
+  <*> optional (OA.argument OA.str
+      ( OA.help "Evaluate the file at this path"
+     <> OA.metavar "FILENAME") )
+
 
 main :: IO ()
-main = do
-  filename <- listToMaybe <$> getArgs
-  case filename of
-    Just name -> readFile name >>= flip evalString' []
-    Nothing   -> repl
+main = OA.execParser opts >>= triggerRepl
+  where
+    opts = OA.info (OA.helper <*> parseArgs)
+      ( OA.fullDesc
+     <> OA.progDesc "Read Eval Print Loop for -"
+     <> OA.header "dashrepl - a REPL for -" )
 
-repl :: IO ()
-repl = do
+triggerRepl :: Arguments -> IO ()
+triggerRepl args = do
+  case filename args of
+    Just name -> readFile name >>= flip (evalString' args) []
+    Nothing   -> repl args
+
+repl :: Arguments -> IO ()
+repl args = do
   hSetBuffering stdout NoBuffering
   putStrLn "λ Welcome to dash! λ"
   putStrLn "Type 'quit' to exit."
@@ -48,16 +71,18 @@ repl = do
         Nothing     -> return ()
         Just "quit" -> liftIO exitSuccess
         Just "exit" -> liftIO exitSuccess
-        Just input  -> evalString input --liftIO $ handleInterrupt (return ()) (evalString input)
+        Just input  -> evalString args input --liftIO $ handleInterrupt (return ()) (evalString input)
 
-evalString :: String -> StateT [(String, Literal)] (InputT IO) ()
-evalString ":let" = do
+evalString :: Arguments -> String -> StateT [(String, Literal)] (InputT IO) ()
+evalString _ ":let" = do
   st <- get
   liftIO . putStrLn $ show st
-evalString (stripPrefix ":let " -> Just newbinding) = do
+evalString args (stripPrefix ":let " -> Just newbinding) = do
   let (name, binding) = second (dropWhile (==' ')) $ break (==' ') newbinding
   st <- get
-  let evaled = runEval binding st
+  let parsed = parse binding
+      evaled = runEval parsed st
+  when (showParse args) (liftIO . putStrLn . ppShow $ parsed)
   case evaled of
     Success s' -> case s' of
       Just y -> do
@@ -66,12 +91,14 @@ evalString (stripPrefix ":let " -> Just newbinding) = do
         liftIO . print $ x
       Nothing -> return ()
     Failure d -> liftIO . putStrLn $ show d
-evalString s = liftIO . evalString' s =<< get
+evalString args s = liftIO . evalString' args s =<< get
 
 -- | Evaluate a String of dash code with some extra "stuff" in the environment.
-evalString' :: String -> [(String, Literal)] -> IO ()
-evalString' s st = do
-  let evaled = runEval s st
+evalString' :: Arguments -> String -> [(String, Literal)] -> IO ()
+evalString' args s st = do
+  let parsed = parse s
+      evaled = runEval parsed st
+  when (showParse args) (liftIO . putStrLn . ppShow $ parsed)
   liftIO $ putStrLn $ case evaled of
     Success s' -> colorize (show s')
     Failure d -> show d
@@ -85,5 +112,8 @@ colorize =
     ""
     False
 
-runEval :: String -> [(String, Literal)] -> Result (Maybe Literal)
-runEval s env = eval (Environment env) <$> parseString (runParser expression) mempty s
+runEval :: Result (Term String) -> [(String, Literal)] -> Result (Maybe Literal)
+runEval p env = eval (Environment env) <$> p
+
+parse :: String -> Result (Term String)
+parse s = parseString (runParser expression) mempty s

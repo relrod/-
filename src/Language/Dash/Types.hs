d0665aad70d47f05c9642de2b33de55c1a39f078
Ricky Elrod <ricky@elrod.me>
2016-07-04 04:38:42 -0400

    Make `Eq` mean "is alpha-equivalent to" (i.e. same de Bruijn representation)
    Signed-off-by: Ricky Elrod <ricky@elrod.me>

--- src/Language/Dash/Types.hs
+++ src/Language/Dash/Types.hs
@@ -15,12 +15,17 @@ data Nameless
   = NVar Int
   | NAbs String Nameless
   | NApp Nameless Nameless
-  deriving Eq
 
 instance Show Nameless where
   show (NVar s) = show s
   show (NAbs _ t) = "/. " ++ show t
   show (NApp t1 t2) = show t1 ++ " " ++ show t2
+
+instance Eq Nameless where
+  NVar x == NVar y = x == y
+  NAbs _ x == NAbs _ y = x == y
+  NApp t1 t2 == NApp t3 t4 = t1 == t3 && t2 == t4
+  _ == _ = False
 
 -- TODO: newtype + Reader monad
 type Context = [String]
